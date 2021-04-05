6532935ab99b866b7a60d338551bb352aeafe20b
Josh Hamilton <nearbycoder@gmail.com>
2021-04-05 11:17:56 -0500

    export theme and update version

---
+++ lib/ThemeProvider.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+
+export const ThemeProvider = ({ children }) => {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+};
