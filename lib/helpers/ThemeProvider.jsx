7b70bbc0459390b8b6fabec814f38d13fc1199bf
Josh Hamilton <nearbycoder@gmail.com>
2021-04-06 15:59:08 -0500

    split up files

--- lib/helpers/ThemeProvider.jsx
+++ lib/helpers/ThemeProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme';
+import theme from '../theme';
 
 export const ThemeProvider = ({ children }) => {
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
