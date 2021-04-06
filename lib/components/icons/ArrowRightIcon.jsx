c02d5ed168dd465221fca97b1ee23627b10c48ac
Josh Hamilton <nearbycoder@gmail.com>
2021-04-06 15:37:24 -0500

    add icon example

---
+++ lib/components/icons/ArrowRightIcon.jsx
@@ -0,0 +1,12 @@
+import { createIcon } from '@chakra-ui/react';
+
+export const ArrowRightIcon = createIcon({
+  displayName: 'ArrowRightIcon',
+  viewBox: '0 0 16 16',
+  path: (
+    <path
+      d="M0 8.99408L0 7.00592H12.1183L6.57988 1.42012L8 0L16 8L8 16L6.57988 14.5799L12.1183 8.99408H0Z"
+      fill="currentColor"
+    />
+  ),
+});
