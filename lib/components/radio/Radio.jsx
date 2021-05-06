e7e14c108e300ec86da5c1a2cb4e97989ef24dff
Josh Hamilton <nearbycoder@gmail.com>
2021-05-06 13:01:06 -0500

    DS: Build Radio (#5)
    * add checkbox component
    
    * add radio buttons
    
    * add helper text example, fix alignment, add not-allowed cursor
    
    * fix indeterminate focus state
    
    * add variant for form label
    
    * use group hover
    
    * add nested group selectors
    
    * fix readonly
    
    * update radio
    
    * update font theme aware
    
    * increase spacing and add cursor
    
    * fix hover

---
+++ lib/components/radio/Radio.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Radio as ChakraRadio,
+  useFormControlContext,
+  Box,
+} from '@chakra-ui/react';
+
+export function Radio({ children, ...props }) {
+  const field = useFormControlContext();
+
+  let fieldProps = {};
+  if (field) {
+    fieldProps.isDisabled = field.isDisabled;
+    fieldProps.isReadOnly = field.isReadOnly;
+    fieldProps.isRequired = field.isRequired;
+    fieldProps.isInvalid = field.isInvalid;
+  }
+
+  return (
+    <Box
+      display="inline-block"
+      className="chakra-radio"
+      role="group"
+      sx={
+        (props.isReadOnly || fieldProps.isReadOnly) && {
+          '.chakra-radio__label': {
+            color: 'grey.70',
+            cursor: 'not-allowed',
+          },
+        }
+      }>
+      <ChakraRadio {...fieldProps} {...props}>
+        {children}
+      </ChakraRadio>
+    </Box>
+  );
+}
