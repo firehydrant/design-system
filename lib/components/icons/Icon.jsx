3ad098876cc7c6773be1e511e90245285b36639e
Josh Hamilton <nearbycoder@gmail.com>
2021-05-07 11:02:59 -0500

    Add more icons to icon component (#10)
    * add additional icons and remove broken link
    
    * update example description

--- lib/components/icons/Icon.jsx
+++ lib/components/icons/Icon.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { AccountIcon } from './AccountIcon';
 import { ArrowDownIcon } from './ArrowDownIcon';
 import { ArrowLeftIcon } from './ArrowLeftIcon';
 import { ArrowRightIcon } from './ArrowRightIcon';
 import { ArrowUpIcon } from './ArrowUpIcon';
-import { BrokenLinkIcon } from './BrokenLinkIcon';
 import { CheckIcon } from './CheckIcon';
 import { ChevronDownIcon } from './ChevronDownIcon';
 import { ChevronLeftIcon } from './ChevronLeftIcon';
@@ -17,19 +17,24 @@ import { FileIcon } from './FileIcon';
 import { FilterIcon } from './FilterIcon';
 import { ExternalLinkIcon } from './ExternalLinkIcon';
 import { HelpIcon } from './HelpIcon';
+import { ImportIcon } from './ImportIcon';
 import { InfoIcon } from './InfoIcon';
 import { MinusIcon } from './MinusIcon';
-import { MailIcon } from './MailIcon';
+import { EmailIcon } from './EmailIcon';
 import { PlusIcon } from './PlusIcon';
 import { SearchIcon } from './SearchIcon';
 import { SettingsIcon } from './SettingsIcon';
 import { StarFilledIcon } from './StarFilledIcon';
 import { StarOutlineIcon } from './StarOutlineIcon';
 import { SuccessIcon } from './SuccessIcon';
+import { RefreshIcon } from './RefreshIcon';
+import { TeamIcon } from './TeamIcon';
 import { WarningIcon } from './WarningIcon';
 
 export const Icon = ({ name, isIndeterminate, isChecked, ...props }) => {
   switch (name) {
+    case 'account':
+      return <AccountIcon {...props} />;
     case 'arrowDown':
       return <ArrowDownIcon {...props} />;
     case 'arrowLeft':
@@ -38,8 +43,6 @@ export const Icon = ({ name, isIndeterminate, isChecked, ...props }) => {
       return <ArrowRightIcon {...props} />;
     case 'arrowUp':
       return <ArrowUpIcon {...props} />;
-    case 'brokenLink':
-      return <BrokenLinkIcon {...props} />;
     case 'check':
       return <CheckIcon {...props} />;
     case 'chevronDown':
@@ -66,10 +69,12 @@ export const Icon = ({ name, isIndeterminate, isChecked, ...props }) => {
       return <FilterIcon {...props} />;
     case 'help':
       return <HelpIcon {...props} />;
+    case 'import':
+      return <ImportIcon {...props} />;
     case 'info':
       return <InfoIcon {...props} />;
-    case 'mail':
-      return <MailIcon {...props} />;
+    case 'email':
+      return <EmailIcon {...props} />;
     case 'minus':
       return <MinusIcon {...props} />;
     case 'plus':
@@ -84,9 +89,17 @@ export const Icon = ({ name, isIndeterminate, isChecked, ...props }) => {
       return <SuccessIcon {...props} />;
     case 'search':
       return <SearchIcon {...props} />;
+    case 'refresh':
+      return <RefreshIcon {...props} />;
+    case 'team':
+      return <TeamIcon {...props} />;
     case 'warning':
       return <WarningIcon {...props} />;
     default:
       return null;
   }
 };
+
+// Refresh
+// Search
+// Minus
