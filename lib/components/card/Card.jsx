41cfae61cc2f706d91cbeca4fca5cc29626cfe1a
Josh Hamilton <nearbycoder@gmail.com>
2021-05-11 06:51:50 -0500

    DS: Build Card (#11)
    * DS: Build Card
    
    * set a sane default for heading title
    
    * add spacing for flex wrap
    
    * add example card with multiple actions
    
    * use ButtonGroup and add CardActions
    
    * eslint cleanup
    
    * pass props donw

---
+++ lib/components/card/Card.jsx
@@ -0,0 +1,48 @@
+import React, { Children } from 'react';
+import { Text, Box, ButtonGroup } from '../../main';
+import { useStyleConfig } from '@chakra-ui/react';
+
+export function Card({ variant, size, children, ...rest }) {
+  const styles = useStyleConfig('Card', { variant, size });
+
+  return (
+    <Box __css={styles} {...rest}>
+      {children}
+    </Box>
+  );
+}
+
+export function CardHeader({ variant, size, children, ...rest }) {
+  const styles = useStyleConfig('CardHeader', { variant, size });
+
+  return (
+    <Box __css={styles} {...rest}>
+      {children}
+    </Box>
+  );
+}
+
+export function CardTitle({ children, ...rest }) {
+  return (
+    <Text as="h3" size="3" mr="2" mb="1" {...rest}>
+      {children}
+    </Text>
+  );
+}
+
+export function CardActions({ children, ...rest }) {
+  // We don't want to put a ButtonGroup around a single element
+  if (Children.count(children) === 1) {
+    return (
+      <Box mb="1" {...rest}>
+        {children}
+      </Box>
+    );
+  }
+
+  return (
+    <ButtonGroup mb="1" {...rest}>
+      {children}
+    </ButtonGroup>
+  );
+}
