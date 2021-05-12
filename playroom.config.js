4ef81a6cad2b9da794210952205229354ee3334c
Casey Hunt <casey@firehydrant.io>
2021-05-12 14:18:47 -0500

    Adds playroom
    * adds playroom to project -> plan is to deploy to github pages
    * for now includes a build folder in the commit -> ideally, do not
      include this in the repo and have the build step handled by Github
      actions
    * 2 sample snippets included (FPO)

---
+++ playroom.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  components: './lib/main',
+  outputPath: './playroom',
+
+  //   Optional configuration
+  title: 'FireHydrant Design System',
+  frameComponent: './.playroom/FrameComponent.js',
+  widths: [320, 1100, 1400],
+  snippets: './.playroom/snippets.js',
+};
