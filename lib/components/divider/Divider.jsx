27ea223d506d7377ba2b08c9e94abf675bbd50a9
Josh Hamilton <nearbycoder@gmail.com>
2021-05-12 15:48:39 -0500

    update name

--- lib/components/divider/Divider.jsx
+++ lib/components/divider/Divider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Divider as ChakraDivider } from '@chakra-ui/react';
 
-export function Divider({ ...props }) {
-  return <ChakraDivider {...props}></ChakraDivider>;
+export function Divider({ ...rest }) {
+  return <ChakraDivider {...rest}></ChakraDivider>;
 }
