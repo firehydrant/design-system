575a41efd762f88da688a143dc586ad90158c54e
Josh Hamilton <nearbycoder@gmail.com>
2021-05-12 08:11:29 -0500

    DS: Build Divider

---
+++ lib/components/divider/Divider.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Divider as ChakraDivider } from '@chakra-ui/react';
+
+export function Divider({ ...props }) {
+  return <ChakraDivider {...props}></ChakraDivider>;
+}
