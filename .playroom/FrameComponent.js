50e938c538bb305ef2385b54a0cccc7f0b7dd889
Casey Hunt <casey@firehydrant.io>
2021-05-12 16:35:14 -0500

    Remove unneeded import for Playroom

--- .playroom/FrameComponent.js
+++ .playroom/FrameComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../lib/theme';
 
 export default function FrameComponent({ children }) {
