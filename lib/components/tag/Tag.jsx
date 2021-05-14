f51a8d74cb000a047dab5812de06f0eb173b1baf
Casey Hunt <casey@firehydrant.io>
2021-05-14 16:37:28 -0500

    Changes TagLabel to wrap instead of ellipsis

--- lib/components/tag/Tag.jsx
+++ lib/components/tag/Tag.jsx
@@ -3,10 +3,19 @@ import {
   Tag as ChakraTag,
   TagRightIcon as ChakraTagRightIcon,
   TagLeftIcon as ChakraTagLeftIcon,
+  TagLabel as ChakraTagLabel,
 } from '@chakra-ui/react';
 
 export function Tag({ children, ...props }) {
   return <ChakraTag {...props}>{children}</ChakraTag>;
+}
+
+export function TagLabel({ children, ...props }) {
+  return (
+    <ChakraTagLabel textOverflow="unset" whiteSpace="break-spaces" {...props}>
+      {children}
+    </ChakraTagLabel>
+  );
 }
 
 export function TagRightIcon({ children, ...props }) {
