bd4ebc32fec6b003e1247bc8dacc7ee9083d9706
Josh Hamilton <nearbycoder@gmail.com>
2021-06-01 13:08:32 -0500

    use span for correct nesting

--- lib/components/link/LinkText.jsx
+++ lib/components/link/LinkText.jsx
@@ -5,7 +5,7 @@ export function LinkText({ variant, size, children, ...rest }) {
   const styles = useStyleConfig('LinkText', { variant, size });
 
   return (
-    <Box __css={styles} {...rest}>
+    <Box as="span" __css={styles} {...rest}>
       {children}
     </Box>
   );
