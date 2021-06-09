73342f47a34b7c11af622b68a8b6bb11b4d9eaa9
Casey Hunt <casey@firehydrant.io>
2021-06-09 13:52:54 -0500

    Refactor RadioGroup & CheckboxGroup to use <Stack> instead of <Flex>
    This swaps <Flex> for <Stack> in both RadioGroup and CheckboxGroup,
    allowing for cleaner code, removing `__css`, and follows best practice
    according to Chakra.

--- lib/components/checkbox/CheckboxGroup.jsx
+++ lib/components/checkbox/CheckboxGroup.jsx
@@ -1,33 +1,16 @@
 import React from 'react';
-import { Flex, CheckboxGroup as ChakraCheckboxGroup } from '@chakra-ui/react';
+import { Stack, CheckboxGroup as ChakraCheckboxGroup } from '@chakra-ui/react';
 
 export function CheckboxGroup({ children, horizontal, ...props }) {
-  // display: flex needed see issue -> https://github.com/chakra-ui/chakra-ui/issues/2100
   return (
     <ChakraCheckboxGroup {...props}>
-      <Flex
-        __css={
-          horizontal
-            ? {
-                display: 'flex',
-                flexDirection: 'row',
-                '.chakra-checkbox': { mr: 4 },
-                '.chakra-checkbox:last-of-type': {
-                  mr: '0px',
-                },
-              }
-            : {
-                display: 'flex',
-                flexDirection: 'column',
-                '.chakra-checkbox': { mt: 'mg0' },
-                '.chakra-checkbox:first-of-type': {
-                  mt: '0px',
-                },
-              }
-        }
+      <Stack
+        align="stretch"
+        direction={horizontal ? 'row' : 'column'}
+        spacing={horizontal ? '4' : 'mg0'}
       >
         {children}
-      </Flex>
+      </Stack>
     </ChakraCheckboxGroup>
   );
 }
--- lib/components/radio/RadioGroup.jsx
+++ lib/components/radio/RadioGroup.jsx
@@ -1,33 +1,17 @@
 import React from 'react';
-import { Flex, RadioGroup as ChakraRadioGroup } from '@chakra-ui/react';
+import { Flex, Stack, RadioGroup as ChakraRadioGroup } from '@chakra-ui/react';
 
 export function RadioGroup({ children, horizontal, ...props }) {
   // display: flex needed see issue -> https://github.com/chakra-ui/chakra-ui/issues/2100
   return (
     <ChakraRadioGroup {...props}>
-      <Flex
-        __css={
-          horizontal
-            ? {
-                display: 'flex',
-                flexDirection: 'row',
-                '.chakra-radio': { mr: 4 },
-                '.chakra-radio:last-of-type': {
-                  mr: '0px',
-                },
-              }
-            : {
-                display: 'flex',
-                flexDirection: 'column',
-                '.chakra-radio': { mt: 'mg0' },
-                '.chakra-radio:first-of-type': {
-                  mt: '0px',
-                },
-              }
-        }
+      <Stack
+        align="stretch"
+        direction={horizontal ? 'row' : 'column'}
+        spacing={horizontal ? '4' : 'mg0'}
       >
         {children}
-      </Flex>
+      </Stack>
     </ChakraRadioGroup>
   );
 }
