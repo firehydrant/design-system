9a670204e2013265f06fb424b4af4b2d80594fe6
Casey Hunt <casey@firehydrant.io>
2021-08-02 15:52:18 -0500

    feat(icon): adds Home Filled / Outline icons

---
+++ lib/components/icons/HomeFilledIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Icon } from '@chakra-ui/react';
+
+export const HomeFilledIcon = (props) => (
+  <Icon boxSize="24px" viewBox="0 0 24 24" {...props}>
+    <path
+      d="M9.981 21v-6.347h4.038V21h4.976v-8.48H22L12 3 2 12.52h3.005V21H9.98z"
+      fill="currentColor"
+    />
+  </Icon>
+);
