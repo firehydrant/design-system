237326e4d3354c94910b18480d3bf8a6b680e82e
Hilary Beck <42321596+hilaryBeck@users.noreply.github.com>
2021-09-01 15:31:57 -0400

    add closeonblur

--- lib/components/menu/Menu.jsx
+++ lib/components/menu/Menu.jsx
@@ -7,7 +7,7 @@ import {
 
 export function Menu({ buttonComponent, children, maxWidth, ...props }) {
   return (
-    <ChakraMenu preventOverflow { ...props }>
+    <ChakraMenu closeOnBlur preventOverflow { ...props }>
       {/* NOTE: MenuButton will be moved into its own component upon beginning the Button Menu story. */}
       <MenuButton>{buttonComponent}</MenuButton>
       <MenuList maxWidth={maxWidth}>
