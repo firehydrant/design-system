9aa4cf2ddb3ec7257770d9ff62078053beeb6110
hellobrian <brianhan@hey.com>
2021-09-16 11:07:19 -0500

    chore: setup lefthook to trigger prettier and eslint on pre-commit with staged files

--- lib/components/alert/Alert.jsx
+++ lib/components/alert/Alert.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import {Flex, Spacer, useStyleConfig } from '@chakra-ui/react';
+import { Flex, Spacer, useStyleConfig } from '@chakra-ui/react';
 import { Icon, IconButton, Text } from '../../main';
 
 export function Alert({ children, dismissable, onDismiss, variant }) {
   const styles = useStyleConfig('Alert', { variant });
 
   const iconColorScheme = {
-    'alert': 'red.70',
-    'info': 'grey.70',
-    'success': 'green.70',
-    'warning': 'brown.70',
+    alert: 'red.70',
+    info: 'grey.70',
+    success: 'green.70',
+    warning: 'brown.70',
   };
 
   return (
-    <Flex __css={styles} borderWidth="1px" borderStyle="solid" borderRadius="4px" variant={variant}>
+    <Flex
+      __css={styles}
+      borderWidth="1px"
+      borderStyle="solid"
+      borderRadius="4px"
+      variant={variant}
+    >
       <Flex>
         <Flex align="flex-start" py="mg2">
           <Icon name={variant} ml="1" mr="2" color={iconColorScheme[variant]} />
-          <Text color="grey.90" size="5" mb="0">{ children }</Text>
+          <Text color="grey.90" size="5" mb="0">
+            {children}
+          </Text>
         </Flex>
         <Spacer py="mg1" />
-        { dismissable && (
+        {dismissable && (
           <IconButton
             aria-label="close"
             name="close"
