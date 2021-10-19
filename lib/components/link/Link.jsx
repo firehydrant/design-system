64f4600618da598bf2e95dd9f24fb8e2b1275dc8
Casey Hunt <casey@firehydrant.io>
2021-10-19 16:58:00 -0500

    adding proptypes & default values
    also cleans up storybook stories & adds prop-types dependency

--- lib/components/link/Link.jsx
+++ lib/components/link/Link.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link as ChakraLink, useStyleConfig } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '../../main';
 
-export function Link({ children, isExternal, size, variant, ...props }) {
+export function Link({
+  children,
+  isExternal,
+  size = 5,
+  variant = 'primary',
+  ...props
+}) {
   const styles = useStyleConfig('Link', { variant, size });
 
   return (
@@ -20,3 +27,11 @@ export function Link({ children, isExternal, size, variant, ...props }) {
     </ChakraLink>
   );
 }
+
+Link.propTypes = {
+  children: PropTypes.node,
+  isExternal: PropTypes.bool,
+  size: PropTypes.oneOf([5, 6, 7]),
+  variant: PropTypes.oneOf(['primary', 'reverse']),
+  href: PropTypes.string,
+};
