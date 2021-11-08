bb41e6ef59a1b57c59b9e6b8d38c279206d0da37
Casey Hunt <casey@firehydrant.io>
2021-11-08 10:23:35 -0600

    feat(ThemeProvider): adds support for `nonce` key (#159)
    ThemeProvider now accepts a `nonce` key (prop) for the purposes of CSP
    (content security policy).

--- lib/helpers/ThemeProvider.jsx
+++ lib/helpers/ThemeProvider.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 
-export const ThemeProvider = ({ children, ...props }) => {
-  return <ChakraProvider theme={theme} {...props}>{children}</ChakraProvider>;
+function EmotionCacheProvider({ nonce, children }) {
+  const cache = createCache({ key: 'contentsecuritypolicy', nonce });
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
+}
+
+export const ThemeProvider = ({ children, nonce, ...props }) => {
+  return (
+    <EmotionCacheProvider nonce={nonce}>
+      <ChakraProvider theme={theme} {...props}>
+        {children}
+      </ChakraProvider>
+    </EmotionCacheProvider>
+  );
+};
+
+ThemeProvider.propTypes = {
+  nonce: PropTypes.string,
+  children: PropTypes.element,
 };
