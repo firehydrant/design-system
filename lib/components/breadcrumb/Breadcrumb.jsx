3727b032bcd3ea3e581f498d4309f39c04676e16
Hilary Beck <42321596+hilaryBeck@users.noreply.github.com>
2021-11-15 15:36:54 -0500

    finishing breadcrumbs

--- lib/components/breadcrumb/Breadcrumb.jsx
+++ lib/components/breadcrumb/Breadcrumb.jsx
@@ -1,29 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Breadcrumb as ChakraBreadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
-  Link as ChakraLink,
+  BreadcrumbSeparator,
+  useStyleConfig
 } from '@chakra-ui/react';
-import { Icon, IconButtonDropdown, MenuItem } from '../../main';
+import { Icon, IconButtonDropdown, Link as DSLink, MenuItem } from '../../main';
 
 const ItemLink = ({crumb, isCurrentPage}) => {
+  const styles = useStyleConfig('Link');
+
   return (
     <BreadcrumbLink
-      as={ChakraLink}
+      as={DSLink}
       href={crumb.url}
       isCurrentPage={isCurrentPage}
       size={6}
+      __css={styles}
     >
       {crumb.text.replace('_', ' ')}
     </BreadcrumbLink>
   )
 }
 
-const BreadcrumbsListWithDropdown = ({crumbs}) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+const BreadcrumbsList = ({ crumbs}) => {
+  return (
+    <ChakraBreadcrumb separator={<Icon name="chevronRight" color="grey.70" />} spacing="4px">
+      {crumbs.ids.map((crumbId, index) => (
+        <BreadcrumbItem key={crumbId}>
+          <ItemLink
+            crumb={crumbs.breadcrumbs[crumbId]}
+            isCurrentPage={index === crumbs.ids.length - 1} href={crumbs.breadcrumbs[crumbId].url}
+          />
+        </BreadcrumbItem>
+      ))}
+    </ChakraBreadcrumb>
+  )
+}
 
+const BreadcrumbsListWithDropdown = ({ crumbs }) => {
   // Display only first, penultimate, and last breadcrumbs
   const firstCrumb = crumbs.breadcrumbs[crumbs.ids[0]];
   const penultimateCrumb = crumbs.breadcrumbs[crumbs.ids[crumbs.ids.length - 2]];
@@ -37,52 +53,35 @@ const BreadcrumbsListWithDropdown = ({crumbs}) => {
   });
 
   return (
-    <>
-      <BreadcrumbItem>
+    <ChakraBreadcrumb separator={<Icon name="chevronRight" color="grey.70" />} spacing="4px">
+      <BreadcrumbItem key={crumbs.ids[0]}>
         <ItemLink crumb={firstCrumb} />
       </BreadcrumbItem>
       <IconButtonDropdown icon="menu" buttonVariant="tertiary">
-        {dropdownOptions.map(option => 
-          <MenuItem option={option} />
+        {dropdownOptions.map((option, index) => 
+          <MenuItem option={option} key={index} />
         )}
       </IconButtonDropdown>
-      <BreadcrumbItem>
+      <BreadcrumbSeparator spacing="4px">
+        <Icon name="chevronRight" color="grey.70" />
+      </BreadcrumbSeparator>
+      <BreadcrumbItem key={crumbs.ids[crumbs.ids.length - 2]}>
         <ItemLink crumb={penultimateCrumb} />
       </BreadcrumbItem>
-      <BreadcrumbItem>
+      <BreadcrumbItem key={crumbs.ids[crumbs.ids.length - 1]}>
         <ItemLink crumb={lastCrumb} isCurrentPage />
       </BreadcrumbItem>
-    </>
+    </ChakraBreadcrumb>
   )
 }
 
 export function Breadcrumb({ crumbs }) {
-  const renderBreadcrumbsList = (crumbs) => {
-    if (crumbs.ids) {
-      if (crumbs.ids.length <= 3) {
-        return (
-          <>
-            {crumbs.ids.map((crumbId, index) => (
-              <BreadcrumbItem>
-                <ItemLink
-                  crumb={crumbs.breadcrumbs[crumbId]}
-                  isCurrentPage={index === crumbs.ids.length - 1} href={crumbs.breadcrumbs[crumbId].url}
-                  key={crumbId}
-                />
-              </BreadcrumbItem>
-            ))}
-          </>
-        )
-      }
-      if (crumbs.ids.length > 3) {
-        return <BreadcrumbsListWithDropdown crumbs={crumbs} />
-      }
+  if (crumbs.ids) {
+    if (crumbs.ids.length <= 3) {
+      return <BreadcrumbsList crumbs={crumbs}/>
+    }
+    if (crumbs.ids.length > 3) {
+      return <BreadcrumbsListWithDropdown crumbs={crumbs} />
     }
   }
-
-  return (
-    <ChakraBreadcrumb separator={<Icon name="chevronRight" />} spacing="4px">
-      {renderBreadcrumbsList(crumbs)}
-    </ChakraBreadcrumb>
-  )
 }
