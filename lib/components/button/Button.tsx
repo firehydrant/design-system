b08d806cfdf2ab6bb661418e18a661ef85d5aa4a
hellobrian <brianhan@hey.com>
2021-11-15 15:36:42 -0600

    fix(button): overwrite variant to use correct types

--- lib/components/button/Button.tsx
+++ lib/components/button/Button.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Button as ChakraButton, ButtonProps } from '@chakra-ui/react';
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps,
+} from '@chakra-ui/react';
+
+interface ButtonProps extends Omit<ChakraButtonProps, 'variant'> {
+  variant: 'primary' | 'secondary' | 'tertiary';
+}
 
 export function Button(props: ButtonProps) {
   return <ChakraButton my="mg0" iconSpacing={1} {...props} />;
