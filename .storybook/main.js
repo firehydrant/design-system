eba86e0765bcf363c0720b1e1b08ef49a13149ca
hellobrian <brianhan@hey.com>
2022-04-11 18:06:35 -0500

    fix(storybook): add core.builder webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const toPath = (_path) => path.join(process.cwd(), _path);
 
-// seems their is a current issues with webpack, storybook and Chakra deps
+// seems there is a current issue with webpack, storybook and Chakra deps
 // https://github.com/chakra-ui/chakra-ui/issues/2527#issuecomment-810293915
 module.exports = {
   stories: ['../lib/**/*.stories.mdx', '../lib/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#from-version-62x-to-630
+  core: {
+    builder: 'webpack5',
+  },
   webpackFinal: async (config) => {
     return {
       ...config,
