a91a7e0e55965677a425d86bedfe2e912b433c88
hellobrian <brianhan@hey.com>
2022-04-11 17:45:59 -0500

    chore(Table): add a basic test

---
+++ lib/components/table/Table.spec.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { Thead, Tbody, Tr, Th, Table } from './index';
+
+describe('Table', () => {
+  it('should render', () => {
+    render(
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>name</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Th>brian</Th>
+          </Tr>
+        </Tbody>
+      </Table>,
+    );
+    expect(screen.getByText('name')).toBeInTheDocument();
+  });
+});
